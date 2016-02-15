161076b4fcb4052cebd9e03020e8042ce8bcb3a0
ilyamodder <ilyamodder@gmail.com>
2016-02-15 14:05:55 +0300

    Moved to Retrofit call

--- 2sem/09.02.16-intentservice/app/src/main/java/ru/ilyamodder/intentservicetest/service/OpenWeatherMapApi.java
+++ 2sem/09.02.16-intentservice/app/src/main/java/ru/ilyamodder/intentservicetest/service/OpenWeatherMapApi.java
@@ -1,5 +1,6 @@
 package ru.ilyamodder.intentservicetest.service;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import ru.ilyamodder.intentservicetest.classes.Weather;
@@ -10,5 +11,5 @@ import ru.ilyamodder.intentservicetest.classes.Weather;
 public interface OpenWeatherMapApi {
 
     @GET("/forecast?mode=json")
-    Weather get5DaysForecast(@Query("city") String city);
+    Call<Weather> get5DaysForecast(@Query("city") String city);
 }
